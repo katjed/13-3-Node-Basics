f8e2cac938bab89cccb834b7cf1171485ebcb322
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-11 01:17:03 +0100

    Add js

---
+++ program.js
@@ -0,0 +1,44 @@
+// console.log(global);
+// console.log(process.versions);
+// console.log(process.env);
+
+process.stdin.setEncoding('utf-8');
+
+/*
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
+*/
+
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+  if (input !== null) {
+	process.stdout.write(input);
+    var instruction = input.toString().trim();
+    switch (instruction) {
+    	case 'version':
+        	process.stdout.write('Node version: ' + process.versions.node + '\n');
+        	break;
+      	case 'language':
+        	process.stdout.write('Language: ' + process.env.LANG + '\n');
+        	break;
+      	case '/exit':
+        	process.stdout.write('Quitting app!\n');
+        	process.exit();
+        	break;
+      	default:
+        	process.stderr.write('Wrong instruction!\n');
+    	}
+  	}
+});
+
+
