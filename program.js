4be80d6e9c93b302dadf7f967e02a9f0eb7e111a
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-02-24 23:54:47 +0100

    Update js

--- program.js
+++ program.js
@@ -6,16 +6,16 @@ process.stdin.setEncoding('utf-8');
 
 /*
 process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!\n');
-        }
-    }
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		if (instruction === '/exit') {
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+		} else {
+			process.stderr.write('Wrong instruction!\n');
+		}
+	}
 });
 */
 
@@ -23,22 +23,22 @@ process.stdin.on('readable', function() {
   var input = process.stdin.read();
   if (input !== null) {
 	process.stdout.write(input);
-    var instruction = input.toString().trim();
-    switch (instruction) {
-    	case 'version':
-        	process.stdout.write('Node version: ' + process.versions.node + '\n');
-        	break;
-      	case 'language':
-        	process.stdout.write('Language: ' + process.env.LANG + '\n');
-        	break;
-      	case '/exit':
-        	process.stdout.write('Quitting app!\n');
-        	process.exit();
-        	break;
-      	default:
-        	process.stderr.write('Wrong instruction!\n');
-    	}
-  	}
+	var instruction = input.toString().trim();
+	switch (instruction) {
+		case '/exit':
+			process.stdout.write('Quitting app!\n');
+			process.exit();
+			break;
+		case '/version':
+			process.stdout.write('Node version: ' + process.versions.node + '\n');
+			break;
+		case '/language':
+			process.stdout.write('Language: ' + process.env.LANG + '\n');
+			break;		
+		default:
+			process.stderr.write('Wrong instruction!\n');
+		}
+	}
 });
 
 
